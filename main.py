431c0b243538882e587c1b420cffbacb24d47ddc
kosvish <mr.kocta@bk.ru>
2023-01-14 20:22:16 +0300

    We create a variable with the path to our music, and a box sheet with music

--- main.py
+++ main.py
@@ -8,4 +8,10 @@ canvas.title('Music Player')
 canvas.geometry('600x800')
 canvas.config(bg='black')
 
+rootpath = "C:\\Users\\Константин\\Desktop\\music"
+pattern = "*.mp3"
+
+listBox = tk.Listbox(canvas, fg='cyan', bg="black", width=100)
+listBox.pack(padx=15, pady=15)
+
 canvas.mainloop()
