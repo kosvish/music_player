c8169d31c60d78bde9e778930e9ba29b33d220d3
kosvish <mr.kocta@bk.ru>
2023-01-14 21:25:06 +0300

    Create a couple of buttons: Stop and Prev

--- main.py
+++ main.py
@@ -14,9 +14,21 @@ pattern = "*.mp3"
 listBox = tk.Listbox(canvas, fg='cyan', bg="black", width=100, font=('Sonic 1 Title Screen Outline', 10))
 listBox.pack(padx=15, pady=15)
 
+label = tk.Label(canvas, text='', bg='black', fg='yellow', font=('Sonic 1 Title Screen Outline', 18))
+label.pack(pady=15)
+
+prevButton = tk.Button(canvas, text='Prev')
+prevButton.pack(pady=15)
+
+stopButton = tk.Button(canvas, text='Stop')
+stopButton.pack(pady=15)
+
+
 for root, dirs, files in os.walk(rootpath):
     for filename in fnmatch.filter(files, pattern):
         listBox.insert('end', filename)
+
+
 
 
 canvas.mainloop()
