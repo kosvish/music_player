8f0b7a14d235616d26cfa33dd62f9b0766858590
kosvish <mr.kocta@bk.ru>
2023-01-14 21:16:40 +0300

    Download the style for our listbox from the Internet, specify the name

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ canvas.config(bg='black')
 rootpath = "C:\\Users\\Константин\\Desktop\\music"
 pattern = "*.mp3"
 
-listBox = tk.Listbox(canvas, fg='cyan', bg="black", width=100)
+listBox = tk.Listbox(canvas, fg='cyan', bg="black", width=100, font=('Sonic 1 Title Screen Outline', 14))
 listBox.pack(padx=15, pady=15)
+
+listBox.insert(0, 'Coding')
+listBox.insert(1, 'Width')
+listBox.insert(2, 'Kostya')
 
 canvas.mainloop()
